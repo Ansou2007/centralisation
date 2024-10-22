872a72acc88569fc5807fd6b793f32f722369289
Ansoumane Michel TAMBA <ansoumanemichel.tamba@unchk.edu.sn>
2024-10-22 14:42:22 +0000

    Correctif

--- resources/views/layouts/Header.blade.php
+++ resources/views/layouts/Header.blade.php
@@ -2,8 +2,8 @@
  <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-      <a href="index.html" class="logo d-flex align-items-center">
-        <img src="assets/img/logo.png" alt="">
+      <a href="" class="logo d-flex align-items-center">
+        <img src="{{asset('assets/img/logo_unchk.jpeg')}}" alt="" height="200" width="50">
         <span class="d-none d-lg-block">Centralisation</span>
       </a>
       <i class="bi bi-list toggle-sidebar-btn"></i>
